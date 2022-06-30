6f39ae31f1bd5f57df115218dd4ffdf06bdb2f45
Ruby Izekor <1izekorruby@gmail.com>
2022-06-30 17:03:20 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-import sys
-
-
-length = len(sys.argv)
-if length > 0:
-    print("{:d} arguments:".format(length - 1))
-    for i in range(1, length):
-        print("{:d}: {:s}".format(i, sys.argv[i]))
-
-else:
-    print("0 arguments.")
+    import sys
+    
+    
+    length = len(sys.argv)
+    if length > 0:
+        print("{:d} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            
+    else:
+        print("0 arguments.")
