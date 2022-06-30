21f122d2491e804f0dc46a8b72d19e97b354c591
Ruby Izekor <1izekorruby@gmail.com>
2022-06-30 17:05:59 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-import sys
-
-length = len(sys.argv)
-num = 0
-
-for i in range(1, length):
-    num += int(sys.argv[i])
-
-print(f"{num}")
+if __name__ == "__main__":
+    import sys
+    
+    length = len(sys.argv)
+    num = 0
+    for i in range(1, length):
+        num += int(sys.argv[i])
+        
+    print("{:d}".format(num))
