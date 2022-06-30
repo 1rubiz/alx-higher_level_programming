29b0826cb9a99bbb4886e6f703ae6c49497939ad
1rubiz <1izekorruby@gmail.com>
2022-06-30 08:07:08 -0700

    python imports and modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+
+length = len(sys.argv)
+if length > 0:
+    print(f"{length - 1} arguments:")
+    for i in range(1, length):
+        print(f"{i}: {sys.argv[i]}")
+
+else:
+    print(f"0 arguments.")
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+length = len(sys.argv)
+num = 0
+
+for i in range(1, length):
+    num += int(sys.argv[i])
+
+print(f"{num}")
