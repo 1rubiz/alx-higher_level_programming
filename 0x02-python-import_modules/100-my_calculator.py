b212528e409af90dcd2158ebbf3e4ad5c124ca8d
1rubiz <1izekorruby@gmail.com>
2022-06-30 11:15:41 -0700

    My Python Terminal Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,17 +10,17 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            operator = sys.argv[2]
-            if operator == '+':
-                print(f"{a} {operator} {b} = {add(a, b)}")
-            elif operator == '-':
-                print(f"{a} {operator} {b} = {sub(a, b)}")
-            elif operator == '*':
-                print(f"{a} {operator} {b} = {mul(a, b)}")
-            elif operator == '/':
-                print(f"{a} {operator} {b} = {div(a, b)}")
-            else:
-                print(f"Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        operator = sys.argv[2]
+        if operator == "+":
+            print(f"{a} {operator} {b} = {add(a, b)}")
+        elif operator == "-":
+            print(f"{a} {operator} {b} = {sub(a, b)}")
+        elif operator == "*":
+            print(f"{a} {operator} {b} = {mul(a, b)}")
+        elif operator == "/":
+            print(f"{a} {operator} {b} = {div(a, b)}")
+        else:
+            print(f"Unknown operator. Available operators: +, -, * and /")
+            exit(1)
