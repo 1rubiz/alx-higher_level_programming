5c905b1cc3a8bd66fb47fcb66dfca95f1327b718
Ruby Izekor <1izekorruby@gmail.com>
2022-07-03 17:35:27 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,9 +5,10 @@ def element_at(my_list, idx):
     length = len(my_list)
     value = my_list[idx]
 
-    if idx < 0:
-        return None
-    elif idx > length:
-        return None
-    else:
-        return value
+    for i in range(length):
+        if idx < 0:
+            return None
+        elif idx > length:
+            return None
+        else:
+            return value
