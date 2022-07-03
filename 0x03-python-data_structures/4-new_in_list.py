ddd33408effef069c4f1321c1e621d46a32b9540
Ruby Izekor <1izekorruby@gmail.com>
2022-07-03 18:06:28 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,7 +8,7 @@ def new_in_list(my_list, idx, element):
 
     if idx < 0:
         return my_list
-    if idx > length:
+    if idx > (length - 1):
         return my_list
     else:
         temp[idx] = element
