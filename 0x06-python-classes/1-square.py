d5d0e6420972473fa2c68658d917f44398cbb9a3
1rubiz <1izekorruby@gmail.com>
2022-07-19 10:01:05 -0700

    Square class with a private attribute declaration

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Class Square: Creates a square class with a size
+variable.
+"""
+
+
+class Square:
+    """square class with a size variable"""
+
+    def __init__(self, size):
+        """variable declaration"""
+        self.__size = size
