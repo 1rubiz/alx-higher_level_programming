9b5a2c3a03bfac050d9f48245750604329164299
1rubiz <1izekorruby@gmail.com>
2022-07-19 10:12:35 -0700

    conditional statement on the square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Class Square: square class with conditional
+statements
+"""
+
+
+class Square:
+    """class square define a square"""
+
+    def __init__(self, size=0):
+        """conditional statements for the size variable"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
