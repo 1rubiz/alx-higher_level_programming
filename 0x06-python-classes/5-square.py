3118ee343b11e9c35a74b386adfcefe0ea766017
1rubiz <1izekorruby@gmail.com>
2022-07-19 10:58:31 -0700

    Add a create square method with # using the size attribute

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Class Square: square class with conditional
+statements
+"""
+
+
+class Square:
+    """class square define a square"""
+
+    def __init__(self, size=0):
+        """conditional statements for the size variable"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
+
+    @property
+    def size(self):
+        """retrives the current private size attribute"""
+        return self__size
+
+    @size.setter
+    def size(self, value):
+        """set the size attribute value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(size):
+        """returns the square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print a square with # to stdout with the
+        size attribute value"""
+        if self.size != 0:
+            for i in range(self.size):
+                for i in range(self.size):
+                    print('*', end = '')
+                    print()
+        else:
+            print('')
