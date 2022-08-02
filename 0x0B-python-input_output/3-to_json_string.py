d5e366705c67ccf17aaacd8b382d528cbf3fc8b0
1rubiz <1izekorruby@gmail.com>
2022-08-02 16:07:35 -0700

    json to string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module for json conversion to obj"""
+import json
+
+
+def to_json_string(my_obj):
+    """function returns the json rep of the object"""
+    return json.dumps(my_obj)
