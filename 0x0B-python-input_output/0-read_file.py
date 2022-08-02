c61f20877de2f5cad39297c0dda88a57cb758409
1rubiz <1izekorruby@gmail.com>
2022-08-02 15:29:16 -0700

    File input and output in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""File reading program"""
 
 
 def read_file(filename=""):
+    """Function that reads a file to stdout"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
