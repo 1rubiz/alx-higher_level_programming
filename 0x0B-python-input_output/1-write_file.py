cefd0ec4021c721f585b677213650996a6cc12f6
Ruby Izekor <1izekorruby@gmail.com>
2022-08-02 23:51:10 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,9 +4,5 @@
 
 def write_file(filename="", text=""):
     """function that writes into a file"""
-    num = 0
     with open(filename, "r+", encoding="utf-8") as f:
-        f.write(text)
-        for i in f:
-            num += 1
-        return num
+        return(f.write(text))
