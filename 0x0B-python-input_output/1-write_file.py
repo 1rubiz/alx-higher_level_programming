2684f69e3146ccfa5a0caecba96e45c2a691ffaf
1rubiz <1izekorruby@gmail.com>
2022-08-02 15:52:19 -0700

    Function to write into a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """function that writes into a file"""
     with open(filename, "r+", encoding="utf-8") as f:
-        return(f.write(text))
+        return f.write(text)
