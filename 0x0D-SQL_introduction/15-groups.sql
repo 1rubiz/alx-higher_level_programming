2aaee1c37d932935e05455e38e65f31811fc5798
Ruby Izekor <1izekorruby@gmail.com>
2022-09-06 18:48:39 +0100

    manipulating the database

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- A script that lists the number of records with the same score in the table second_table of the database
+--The result should display the score
+-- the number of records for this score with the label number
+-- The list should be sorted by the number of records (descending)
