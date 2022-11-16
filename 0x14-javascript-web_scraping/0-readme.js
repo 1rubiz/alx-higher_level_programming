41dc4f95a47fd1b98c7835172a21285d1e7e6545
Ruby Izekor <1izekorruby@gmail.com>
2022-11-16 05:28:51 +0100

    change readfilr

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-/* script that reads and prints the content of a file
-- first argument is the file path
-- The content of the file must be read in utf-8
-- If an error occurred during the reading, print the error object */
 const filePath = process.argv[2];
 const fs = require('fs');
 
@@ -10,6 +6,6 @@ fs.readFile(filePath, 'utf-8', (error, data)=> {
   if (error) {
     console.log(error);
   } else {
-    console.log(data.toString());
+    console.log(data);
   }
 });
