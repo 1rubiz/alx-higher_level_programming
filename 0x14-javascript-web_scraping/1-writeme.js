5f014fb2dc4524e14f5eeb4ba3f7ea27c17526b1
Ruby Izekor <1izekorruby@gmail.com>
2022-11-16 05:22:36 +0100

    change file type

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-/* script that writes a string to a file
-- The first argument is the file path
-- The second argument is the string to write
-- The content of the file must be read in utf-8
-If an error occurred during the reading, print the error object */
-const args = process.argv;
+const filePath = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(args[2], 'utf-8', (err, data) =>{
+fs.readFile(filePath, 'utf-8', (error, data) =>{
   if (error) { console.log(error); } else { console.log(data); }
 });
