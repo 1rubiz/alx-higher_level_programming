42ab99514a4f9e8466f9a41a4d50edd4563a6865
Ruby Izekor <1izekorruby@gmail.com>
2022-11-16 05:35:03 +0100

    change to string

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,5 +3,5 @@ const filePath = process.argv[2];
 const fs = require('fs');
 
 fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) { console.log(error); } else { console.log(data); }
+  if (error) { console.log(error); } else { console.log(data.toString()); }
 });
