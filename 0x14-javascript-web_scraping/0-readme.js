60299753bf9098cf41918ca344e4f88cbf8ee9f0
Ruby Izekor <1izekorruby@gmail.com>
2022-11-16 05:32:34 +0100

    change readfile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,6 @@
 const filePath = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(filePath, 'utf-8', (error, data)=> {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
+fs.readFile(filePath, 'utf-8', (error, data) => {
+  if (error) { console.log(error); } else { console.log(data); }
 });
