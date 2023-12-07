cff38b1d8a104b777099aab278e7b1f46f7df2bd
Ruby Izekor <1izekorruby@gmail.com>
2023-12-07 11:41:10 +0100

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const filePath = process.argv[2];
-const fs = require('fs');
-
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) { console.log(error); } else { console.log(data.toString()); }
-});
